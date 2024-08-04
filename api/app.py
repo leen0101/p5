22458d38021beff9d4e98d0757b8a09174d6f1d9
Leen0101 <yolkin.cocud@gmail.com>
2024-08-04 12:46:34 +0200

    feat(api): implement text to tag prediction endpoint + base supervised model

--- api/app.py
+++ api/app.py
@@ -21,3 +21,33 @@ def load_model_objects():
     with open('../notebooks/model/top_tags.pkl', 'rb') as f:
         top_tags = pickle.load(f)
     return vectorizer, svd, top_tags
+
+
+X, y = load_data()
+vectorizer, svd, top_tags = load_model_objects()
+
+model = tf.keras.models.load_model('../notebooks/best_model.keras')
+
+
+def transform_text_to_bow(text):
+    X_bow = vectorizer.transform([text])
+    return svd.transform(X_bow)
+
+
+@app.route('/predict_tags', methods=['POST'])
+def predict_tags():
+    question_text = request.json.get('question_text')
+    if not question_text:
+        return jsonify({'error': 'No question text provided'}), 400
+
+    question_vector = transform_text_to_bow(question_text)
+    predicted_tags_probabilities = model.predict(question_vector)
+    predicted_tags = (predicted_tags_probabilities > 0.1).astype(int)
+    predicted_tag_names = [top_tags[i] for i in range(
+        len(predicted_tags[0])) if predicted_tags[0][i] == 1]
+
+    return jsonify({'predicted_tags': predicted_tag_names, 'probabilities': predicted_tags_probabilities.tolist()})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
